e759aaf88eec746c7ae23484738bd87fda552d0c
hrushikeshgaware426 <72458157+Hrushikesh425@users.noreply.github.com>
2026-06-07 00:11:14 +0530

    Fix inconsistent JVM target compatibility for plugins

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,15 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+subprojects {
+    afterEvaluate {
+        tasks.withType<JavaCompile>().configureEach {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions.jvmTarget = "17"
+        }
+    }
+}
