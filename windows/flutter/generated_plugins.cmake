31f6652e584c0609f1bf63ba183d39e4a31d3d19
hrushikeshgaware426 <72458157+Hrushikesh425@users.noreply.github.com>
2026-06-06 08:48:12 +0530

    Remove 7 unused plugins causing build failures, add flutter clean to CI

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -8,13 +8,10 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_auth
   firebase_core
-  firebase_storage
   flutter_secure_storage_windows
-  flutter_tts
   local_auth_windows
   permission_handler_windows
   share_plus
-  speech_to_text_windows
   url_launcher_windows
 )
 
